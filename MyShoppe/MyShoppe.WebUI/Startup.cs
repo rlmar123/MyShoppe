860a7de208691570c7514c76856f382939305a5d
Rafael Lopez <rlmar123@yahoo.com>
2020-10-29 11:22:37 -0400

    Init solution setuo

---
+++ MyShoppe/MyShoppe.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShoppe.WebUI.Startup))]
+namespace MyShoppe.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
